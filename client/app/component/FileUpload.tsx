b0362a59829b2015ee4904c2b593f35264cf0bab
netizen-om <borisagarom@gmail.com>
2025-08-08 17:58:03 +0530

    add handleFileUpload event listner

--- client/app/component/FileUpload.tsx
+++ client/app/component/FileUpload.tsx
@@ -4,10 +4,25 @@ import { Upload } from 'lucide-react';
 
 
 const FileUpload : React.FC = () => {
+
+    const handleFileUploadButtonClick = () => {
+        const ele = document.createElement('input');
+        ele.setAttribute('type', 'file');
+        ele.setAttribute('accept', 'application/pdf');
+        ele.addEventListener('change', (e) => {
+
+            if (ele.files && ele.files.length > 0) {
+                const file = ele.files.item(0);
+                
+            }
+        })
+        ele.click();
+    }
+
   return (
     <>
         <div className='bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-2xl border-1 border-white'>
-            <div className='flex justify-center items-center gap-2 flex-col '>
+            <div onClick={handleFileUploadButtonClick} className='flex justify-center items-center gap-2 flex-col '>
                 <h3>Upload you PDF</h3>
                 <Upload />
             </div>
