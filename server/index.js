0a1334b88aa631ded750892d346713cffd2ecb85
netizen-om <borisagarom@gmail.com>
2025-08-08 18:15:58 +0530

    server config done and server is starting

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const port = 8000
+
+app.use(cors())
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
