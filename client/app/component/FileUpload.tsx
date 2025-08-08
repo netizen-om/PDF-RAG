8b534ef8dbf4d1ffbbf6cede3d1982082497ed79
netizen-om <borisagarom@gmail.com>
2025-08-08 17:49:37 +0530

    add upload PDF component

--- client/app/component/FileUpload.tsx
+++ client/app/component/FileUpload.tsx
@@ -1,9 +1,18 @@
 "use client"
 import React from 'react'
+import { Upload } from 'lucide-react';
+
 
 const FileUpload : React.FC = () => {
   return (
-    <div>FileUpload</div>
+    <>
+        <div className='bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-2xl border-1 border-white'>
+            <div className='flex justify-center items-center gap-2 flex-col '>
+                <h3>Upload you PDF</h3>
+                <Upload />
+            </div>
+        </div>
+    </>
   )
 }
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,11 @@
+import FileUpload from "./component/FileUpload";
 
 
 export default function Home() {
   return <div>
 
     <div className="min-h-screen w-full flex">
-      <div className="w-[30vw] min-h-screen ">1</div>
+      <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center"><FileUpload /></div>
       <div className="w-[70vw] min-h-screen border-l-2">2</div>
     </div>
 
