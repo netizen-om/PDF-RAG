9328d433103eb146ee62538723496c6bdea5ef40
netizen-om <borisagarom@gmail.com>
2025-08-08 18:22:35 +0530

    add multer to upload PDF

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from "express"
 import cors from "cors"
+import multer from "multer"
 
 const app = express()
 const port = 8000
+const upload = multer({ dest : 'uploads/' })
 
 app.use(cors())
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('upload/pdf', upload.single('pdf'), (req, res) =>  {
+    return res.json({ message : "File uploaded"})
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
