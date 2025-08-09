0bab8d8711138c6deebb5ae3a9a3494d08dc6f08
netizen-om <borisagarom@gmail.com>
2025-08-09 11:50:09 +0530

    add al langchain packages

--- server/worker.js
+++ server/worker.js
@@ -1,10 +1,13 @@
 import { Worker } from "bullmq";
-import { log } from "console";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 const worker = new Worker(
     'file-upload-queue',
     async(job) => {
         console.log('Job : ', job.data)
+        const data = JSON.parse(job.data);
     },
     {
         concurrency : 100,
