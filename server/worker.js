84b49c7999aabaa0b97041d30f0fc9ab5769eb5e
netizen-om <borisagarom@gmail.com>
2025-08-09 11:35:48 +0530

    configure worker.js

--- server/worker.js
+++ server/worker.js
@@ -1,2 +1,17 @@
 import { Worker } from "bullmq";
+import { log } from "console";
 
+const worker = new Worker(
+    'file-upload-queue',
+    async(job) => {
+        console.log('Job : ', job.data)
+    },
+    {
+        concurrency : 100,
+        connection : {
+            host : 'localhost',
+            port : 6379,
+        },
+    }
+    
+)
