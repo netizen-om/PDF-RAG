f48aa6c8dd4b5457ff2a92519af97d87dfafcaaf
netizen-om <borisagarom@gmail.com>
2025-08-09 13:10:45 +0530

    all bug fixed and data uploaded to qdrant DB

--- server/worker.js
+++ server/worker.js
@@ -6,34 +6,42 @@ import { CharacterTextSplitter } from "@langchain/textsplitters";
 import { log } from "console";
 import { QdrantClient } from "@qdrant/js-client-rest";
 import { GoogleGenerativeAIEmbeddings  }  from "@langchain/google-genai"
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const client = new QdrantClient({ url: "http://localhost:6333" });
 
 const worker = new Worker(
     'file-upload-queue',
     async(job) => {
-        console.log('Job : ', job.data)
-        const data = JSON.parse(job.data);
-
-        //PDF loader
-        const loader = new PDFLoader(data.path);
-        const docs = await loader.load();
-        console.log("DOCS : ", docs);
-        
-       const embeddings = new GoogleGenerativeAIEmbeddings({
-        model : 'embedding-001',
-        apiKey : process.env.GOOGLE_API_KEY
-       });
+        try {
+            console.log('Job : ', job.data)
+            const data = JSON.parse(job.data);
     
-       const vectorStore = await QdrantVectorStore.fromExistingCollection(
-            embeddings,
-            {
-                url : "http://localhost:6333",
-                collectionName : 'pdf-docs'
-            }
-       )
-       await vectorStore.addDocuments(docs);
-       console.log("All docs are added");
+            //PDF loader
+            const loader = new PDFLoader(data.path);
+            const docs = await loader.load();
+            console.log("DOCS : ", docs);
+            
+           const embeddings = new GoogleGenerativeAIEmbeddings({
+            model : 'embedding-001',
+            apiKey : process.env.GOOGLE_API_KEY
+           });
+        
+           const vectorStore = await QdrantVectorStore.fromExistingCollection(
+                embeddings,
+                {
+                    url : "http://localhost:6333",
+                    collectionName : 'pdf-docs'
+                }
+           )
+           await vectorStore.addDocuments(docs);
+           console.log("All docs are added");
+        } catch (error) {
+            console.log("ERROR : ", error);
+            
+        }
        
        
     },
