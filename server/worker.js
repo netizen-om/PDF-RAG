69d2d4a93203f04950fb33eac32cc286be36ee8f
netizen-om <borisagarom@gmail.com>
2025-08-09 11:59:23 +0530

    add PDF loader

--- server/worker.js
+++ server/worker.js
@@ -2,12 +2,25 @@ import { Worker } from "bullmq";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { QdrantVectorStore } from "@langchain/qdrant";
 import { OpenAIEmbeddings } from "@langchain/openai";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 
 const worker = new Worker(
     'file-upload-queue',
     async(job) => {
         console.log('Job : ', job.data)
         const data = JSON.parse(job.data);
+
+        //PDF loader
+        const loader = new PDFLoader(data.path);
+        const docs = await loader.load();
+
+        const textSplitter = new CharacterTextSplitter({
+            chunkSize: 100,
+            chunkOverlap: 0,
+        });
+        const texts = await textSplitter.splitText(docs);
+        console.log(texts);
+        
     },
     {
         concurrency : 100,
