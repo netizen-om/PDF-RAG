795a3600031626910abd50185c892521ddc3cd8c
netizen-om <borisagarom@gmail.com>
2025-08-10 19:05:03 +0530

    changes in FileUpload component

--- client/app/component/FileUpload.tsx
+++ client/app/component/FileUpload.tsx
@@ -1,47 +1,41 @@
-"use client"
-import React from 'react'
+'use client';
+import * as React from 'react';
 import { Upload } from 'lucide-react';
 
+const FileUpload: React.FC = () => {
+  const handleFileUploadButtonClick = () => {
+    const el = document.createElement('input');
+    el.setAttribute('type', 'file');
+    el.setAttribute('accept', 'application/pdf');
+    el.addEventListener('change', async (ev) => {
+      if (el.files && el.files.length > 0) {
+        const file = el.files.item(0);
+        if (file) {
+          const formData = new FormData();
+          formData.append('pdf', file);
 
-const FileUpload : React.FC = () => {
-
-    const handleFileUploadButtonClick = () => {
-        const ele = document.createElement('input');
-        ele.setAttribute('type', 'file');
-        ele.setAttribute('accept', 'application/pdf');
-        ele.addEventListener('change', async(e) => {
-
-            if (ele.files && ele.files.length > 0) {
-                const file = ele.files.item(0);
-
-                if (file) {
-                    const formData = new FormData();
-                    formData.append('pdf', file)
-
-                    await fetch('http://localhost:8000/upload/pdf', {
-                    method : "POST",
-                    body : formData
-                    })
-                    console.log("File uploaded");
-                    
-                }
-                
-                
-            }
-        })
-        ele.click();
-    }
+          await fetch('http://localhost:8000/upload/pdf', {
+            method: 'POST',
+            body: formData,
+          });
+          console.log('File uploaded');
+        }
+      }
+    });
+    el.click();
+  };
 
   return (
-    <>
-        <div className='bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-2xl border-1 border-white'>
-            <div onClick={handleFileUploadButtonClick} className='flex justify-center items-center gap-2 flex-col '>
-                <h3>Upload you PDF</h3>
-                <Upload />
-            </div>
-        </div>
-    </>
-  )
-}
+    <div className="bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-lg border-white border-2">
+      <div
+        onClick={handleFileUploadButtonClick}
+        className="flex justify-center items-center flex-col"
+      >
+        <h3>Upload PDF File</h3>
+        <Upload />
+      </div>
+    </div>
+  );
+};
 
-export default FileUpload
+export default FileUpload;
